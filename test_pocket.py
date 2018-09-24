31f9bac4d85bd4d528e35936511a8b86f16ffa9f
Sunghyun Hwang <me@sunghyunzz.com>
2018-09-24 18:10:49 +0900

    A very simple implementation
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

---
+++ test_pocket.py
@@ -0,0 +1,68 @@
+import unittest
+from typing import List
+
+from pocket import pocket
+
+
+@pocket
+class Apple:
+    a: str
+    b: int
+
+
+@pocket(frozen=True)
+class Banana:
+    apples: List[Apple]
+
+
+@pocket
+class Cat:
+    pass
+
+
+class TestPocket(unittest.TestCase):
+
+    def test_dict(self):
+        self.assertDictEqual(
+            {'a': 'apple', 'b': 3},
+            Apple('apple', 3).dict,
+        )
+
+        self.assertDictEqual(
+            {
+                'apples': [
+                    {'a': '1', 'b': 2},
+                    {'a': '3', 'b': 4},
+                    {'a': '5', 'b': 6}
+                ]
+            },
+            Banana([
+                Apple('1', 2),
+                Apple('3', 4),
+                Apple('5', 6)
+            ]).dict,
+        )
+
+        self.assertDictEqual(
+            {},
+            Cat().dict,
+        )
+
+    def test_json(self):
+        self.assertEqual(
+            '{"a": "abc", "b": 999}',
+            Apple('abc', 999).json
+        )
+
+        self.assertEqual(
+            '{"apples": [{"a": "1", "b": 2}, {"a": "3", "b": 4}]}',
+            Banana([
+                Apple('1', 2),
+                Apple('3', 4)
+            ]).json
+        )
+
+        self.assertEqual(
+            '{}',
+            Cat().json
+        )
